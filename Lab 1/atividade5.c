4094dfed3153acbd28e3f76932c85ef8502e575c
Felipe Melo <felipediasdemelo1@gmail.com>
2021-11-19 15:15:15 -0300

    Versao parcial sem a struct como argumento

--- Lab 1/atividade5.c
+++ Lab 1/atividade5.c
@@ -10,6 +10,13 @@
 #define NTHREADS 2
 #define TAM 10000
 
+typedef struct {
+	int vetor[TAM];
+	int id_thread[NTHREADS]; // identificador local de cada thread
+	int inicio;
+	int fim;
+} args;
+
 
 void preenche_vetor(int *vet) {
 	for (int i = 0 ; i < TAM ; i++)
@@ -23,9 +30,18 @@ void imprime_vetor(int *vet) {
 	printf("\n");
 }
 
-void eleva_quadrado(int *vet, int inicio, int fim) {
-	for (int i = inicio ; i <= fim ; i++)
-		vet[i] = vet[i] * vet[i];
+void *eleva_quadrado(int *vet, int inicio, int fim, void *id_thread) {
+	int id = * (int *) id_thread;
+	if (id == 1) {
+		for (int i = inicio ; i <= fim / 2 ; i++)
+			vet[i] = vet[i] * vet[i];
+	}
+	else if (id == 2) {
+		for (int i = fim / 2 + 1 ; i <= fim ; i++)
+			vet[i] = vet[i] * vet[i];
+	}
+
+	pthread_exit(NULL);
 }
 
 int verifica_valores(int *vet) {
@@ -38,18 +54,24 @@ int verifica_valores(int *vet) {
 
 
 int main() {
-	printf("\n");
 	pthread_t tids_sistema[NTHREADS]; // identificadores das threads no sistema
 	int id_thread[NTHREADS]; // identificador local de cada thread
 	int vetor[TAM];
 	preenche_vetor(vetor);
-	eleva_quadrado(vetor, 0, TAM - 1);
+	
+	for (int i = 0 ; i < NTHREADS ; i++) {
+		id_thread[i] = i;
+		if (pthread_create(&tids_sistema[i], NULL, eleva_quadrado, (void *) &id_thread[i])) {
+			printf(COLOR_BOLD_RED "Erro na funcao pthread_create()\n" COLOR_RESET);
+			exit(1);
+		}
+	}
 
 	if (verifica_valores(vetor))
-		printf(COLOR_BOLD_BLUE "O vetor final esta correto\n\n" COLOR_RESET);
+		printf(COLOR_BOLD_BLUE "O vetor final esta correto\n" COLOR_RESET);
 	else
-		printf(COLOR_BOLD_RED "O vetor final esta errado\n\n" COLOR_RESET);
+		printf(COLOR_BOLD_RED "O vetor final esta errado\n" COLOR_RESET);
 
-	
+	pthread_exit(NULL);
 	return 0;
 }
