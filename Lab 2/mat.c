d156dd42e50922ce448ee8193d74344b55882693
Felipe Melo <felipediasdemelo1@gmail.com>
2021-11-25 18:59:57 -0300

    Aloca e preenche matrizes declaradas globalmente

--- Lab 2/mat.c
+++ Lab 2/mat.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 #include <pthread.h>
 
 #define COLOR_BOLD_BLUE "\e[1;34m"
@@ -9,11 +11,33 @@
 #define COLOR_RESET "\x1b[0m"
 
 typedef struct {
-    int id;
+    int id; // identificador local das trheads
     int dim;
 } Args;
 
-void *multiplica(void *arg) {
+int dimensao, n_threads;
+int **matriz_1;
+int **matriz_2;
+
+// preenche matriz com numeros aleatorios
+void preenche_matriz(int **mat) {
+    srand(time(NULL));
+    for (int i = 0 ; i < dimensao ; i++) {
+        for (int j = 0 ; j < dimensao ; j++)
+            mat[i][j] = rand() % 100; // 0 <= num <= 99
+    }
+}
+
+void imprime_matriz(int **mat) {
+    for (int i = 0 ; i < dimensao ; i++) {
+        printf("["); 
+        for (int j = 0 ; j < dimensao ; j++)
+            printf("%3d", mat[i][j]);
+        printf(" ]\n"); 
+    }
+}
+
+void *multiplica_matrizes(void *arg) {
     Args *args = (Args *) arg;
 }
 
@@ -22,8 +46,25 @@ int main(int argc, char *argv[]) {
         printf(COLOR_BOLD_YELLOW "Execute como: %s <dimensao> <numero de threads>\n" COLOR_RESET, argv[0]);
         return 1;
     }
-    int dimensao = atoi(argv[1]);
-    int n_threads = atoi(argv[2]);
+    dimensao = atoi(argv[1]);
+    n_threads = atoi(argv[2]);
+    if (n_threads > dimensao)
+        n_threads = dimensao;
+    
+    // aloca e preenche a primeira matriz
+    matriz_1 = (int **) malloc(dimensao * sizeof(int*));
+    for (int i = 0 ; i < dimensao ; i++)
+        matriz_1[i] = (int *) malloc(dimensao * sizeof(int*));
+    preenche_matriz(matriz_1);
+
+    sleep(1); // pausa a execucao para gerar matrizes diferentes
+
+    // aloca e preenche a segunda matriz
+    matriz_2 = (int **) malloc(dimensao * sizeof(int*));
+    for (int i = 0 ; i < dimensao ; i++)
+        matriz_2[i] = (int *) malloc(dimensao * sizeof(int*));
+    preenche_matriz(matriz_2);
+
     // pthread_t tids_sistema[NTHREADS];
     // int id_thread[NTHREADS]; // identificador local de cada thread
     return 0;
