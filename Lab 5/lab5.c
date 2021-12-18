d43002579d1b900674540d9acc5609abd1e12bdd
Felipe Melo <56006492+felipsdmelo@users.noreply.github.com>
2021-12-18 15:37:53 -0300

    Trata da thread 5

--- Lab 5/lab5.c
+++ Lab 5/lab5.c
@@ -4,6 +4,57 @@
 
 #define NTHREADS 5
 
+int var = 0; // variavel compartilhada entre as threads
+pthread_mutex_t mutex;  // variavel de exclusao mutua
+pthread_cond_t cond;    // variavel de sincronizacao
+
+void *t1 (void *arg) {
+    printf("(1) Volte sempre!\n");
+    pthread_exit(NULL);
+}
+
+void *t2 (void *arg) {
+    pthread_mutex_lock(&mutex);
+    // espera receber sinal da thread 5
+    while (var < 1) {
+        pthread_cond_wait(&cond, &mutex);
+    }
+    printf("(2) Fique a vontade.\n");
+    pthread_mutex_unlock(&mutex);
+    pthread_exit(NULL);
+}
+
+void *t3 (void *arg) {
+    pthread_mutex_lock(&mutex);
+    // espera receber sinal da thread 5
+    while (var < 1) {
+        pthread_cond_wait(&cond, &mutex);
+    }
+    printf("(3) Sente-se por favor.\n");
+    pthread_mutex_unlock(&mutex);
+    pthread_exit(NULL);
+}
+
+void *t4 (void *arg) {
+    pthread_mutex_lock(&mutex);
+    // espera receber sinal da thread 5
+    while (var < 1) {
+        pthread_cond_wait(&cond, &mutex);
+    }
+    printf("(4) Aceita um copo d'agua?\n");
+    pthread_mutex_unlock(&mutex);
+    pthread_exit(NULL);
+}
+
+void *t5 (void *arg) {
+    pthread_mutex_lock(&mutex);
+    printf("(5) Seja bem-vindo!\n");
+    var++;
+    pthread_cond_broadcast(&cond);
+    pthread_mutex_unlock(&mutex);
+    pthread_exit(NULL);
+}
+
 int main() {
     pthread_t *tids_sistema;
     tids_sistema = (pthread_t *) malloc(NTHREADS * sizeof(pthread_t));
@@ -11,7 +62,21 @@ int main() {
         printf("Erro ao alocar os identificadores das threads do sistema\n");
         exit(1);
     }
+    // int t1 = 1, t2 = 2, t3 = 3, t4 = 4, t5 = 5;
+
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&cond, NULL);
+
+    pthread_create(tids_sistema + 0, NULL, t1, NULL);
+    pthread_create(tids_sistema + 1, NULL, t2, NULL);
+    pthread_create(tids_sistema + 2, NULL, t3, NULL);
+    pthread_create(tids_sistema + 3, NULL, t4, NULL);
+    pthread_create(tids_sistema + 4, NULL, t5, NULL);
 
     free(tids_sistema);
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&cond);
+    
+    pthread_exit(NULL);
     return 0;
 }
