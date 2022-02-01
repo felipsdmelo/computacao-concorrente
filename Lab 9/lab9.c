67beecd16bab772b57a604f15f5f851668fcbd70
Felipe Melo <felipediasdemelo1@gmail.com>
2022-02-01 01:21:07 -0300

    Atualiza logica do programa

--- Lab 9/lab9.c
+++ Lab 9/lab9.c
@@ -3,61 +3,62 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define NTHREADS 5
-
 int var = 0; // variavel compartilhada entre as threads
-sem_t semaforo_1, semaforo_2;
+sem_t semaforo_5, semaforo_1;
 
 void *t1 (void *arg) {
     while (var < 3)
-        sem_wait(&semaforo_2);
+        sem_wait(&semaforo_1);
     printf("(1) Volte sempre!\n");
     pthread_exit(NULL);
 }
 
 void *t2 (void *arg) {
     // espera receber sinal da thread 5
-    sem_wait(&semaforo_1);
+    sem_wait(&semaforo_5);
     printf("(2) Fique a vontade.\n");
     var++;
-    sem_post(&semaforo_2); // envia sinal para a thread 1
+    sem_post(&semaforo_1);
     pthread_exit(NULL);
 }
 
 void *t3 (void *arg) {
     // espera receber sinal da thread 5
-    sem_wait(&semaforo_1);
+    sem_wait(&semaforo_5);
     printf("(3) Sente-se por favor.\n");
     var++;
-    sem_post(&semaforo_2); // envia sinal para a thread 1
+    sem_post(&semaforo_1);
     pthread_exit(NULL);
 }
 
 void *t4 (void *arg) {
     // espera receber sinal da thread 5
-    sem_wait(&semaforo_1);
+    sem_wait(&semaforo_5);
     printf("(4) Aceita um copo d'agua?\n");
     var++;
-    sem_post(&semaforo_2); // envia sinal para a thread 1
+    sem_post(&semaforo_1);
     pthread_exit(NULL);
 }
 
 void *t5 (void *arg) {
     printf("(5) Seja bem-vindo!\n");
-    sem_post(&semaforo_1); // envia sinal para threads 2, 3 e 4
+    // envia sinais para threads 2, 3 e 4
+    sem_post(&semaforo_5);
+    sem_post(&semaforo_5);
+    sem_post(&semaforo_5);
     pthread_exit(NULL);
 }
 
 int main() {
     pthread_t *tids_sistema;
-    tids_sistema = (pthread_t *) malloc(NTHREADS * sizeof(pthread_t));
+    tids_sistema = (pthread_t *) malloc(5 * sizeof(pthread_t));
     if (tids_sistema == NULL) {
         printf("Erro ao alocar os identificadores das threads do sistema\n");
         exit(1);
     }
 
     sem_init(&semaforo_1, 0, 0);
-    sem_init(&semaforo_2, 0, 0);
+    sem_init(&semaforo_5, 0, 0);
 
     pthread_create(tids_sistema + 1, NULL, t2, NULL);
     pthread_create(tids_sistema + 0, NULL, t1, NULL);
@@ -65,6 +66,8 @@ int main() {
     pthread_create(tids_sistema + 4, NULL, t5, NULL);
     pthread_create(tids_sistema + 3, NULL, t4, NULL);
 
+    sem_destroy(&semaforo_1);
+    sem_destroy(&semaforo_5);
     free(tids_sistema);
 
     return 0;
